3417aa0b8db6e29ea2fe905b09a6226b43b07f29
Ängla Juliusson <angla02@icloud.com>
2026-01-16 12:19:12 +0100

    Fungerande nav-länkar i header och tillhörnade styling

--- Lundbergs_Bryggeri/src/components/Header.vue
+++ Lundbergs_Bryggeri/src/components/Header.vue
@@ -2,23 +2,66 @@
 
 <template>
     <header>
-        <div class="menu">
+        <div class="menu-and-button">
             <nav id="main-nav">
                 <ul>
-                    <li>Produkter</li>
-                    <li>Kategorier</li>
-                    <li>Min profil</li>
+                    <li><router-link to="/products">Produkter</router-link></li>
+                    <li><router-link to="/categories">Kategorier</router-link></li>
+                    <li><router-link to="/profile">Min profil</router-link></li>
                 </ul>
             </nav>
+            <button>Logga ut</button>
         </div>
-        <button>Logga ut</button>
     </header>
 </template>
-
+  
 <style scoped>
-    header {
-        background-color: #8B5E3C;
-        padding: 2em;
-        color: #F9F5F0;
-    }
+  header {
+    width: 100%;
+    background-color: #8B5E3C;
+    padding: 2em;
+    box-sizing: border-box;
+  }
+  .menu-and-button {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem; /* mellan menyn och knappen */
+    justify-content: center; /* centrerar hela gruppen i headern */
+    font-size: medium;
+  }
+  #main-nav ul {
+    list-style: none;
+    display: flex;
+    gap: 2rem;
+    padding: 0;
+    margin: 0;
+  }
+  #main-nav ul li a {
+    text-decoration: none;
+    color: #F9F5F0;
+  }
+  button {
+    padding: 0.5em 1em;
+    background-color: #8B5E3C;
+    color: #F9F5F0;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: medium;
+    font-family: monospace;
+  }
+  /* Desktop */
+  @media (min-width: 768px) {
+  .menu-and-button {
+     justify-content: center; /* kan ändras till flex-start om du vill vänsterjustera */
+     gap: 1.5rem; /* mellan menyn och knappen */
+     font-size: large;
+  }
+  #main-nav ul {
+     gap: 3rem;
+  }
+  button {
+    font-size: large;
+  }
+  }
 </style>
