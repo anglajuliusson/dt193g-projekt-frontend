9237fc060c43d765a6cc72eaab4eb3f39fa5bc5a
Ängla Juliusson <angla02@icloud.com>
2026-01-16 11:20:42 +0100

    Header-komponent

---
+++ Lundbergs_Bryggeri/src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+    <header>
+        <div class="menu">
+            <nav id="main-nav">
+                <ul>
+                    <li>Produkter</li>
+                    <li>Kategorier</li>
+                    <li>Min profil</li>
+                </ul>
+            </nav>
+        </div>
+        <button>Logga ut</button>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        background-color: #8B5E3C;
+        padding: 2em;
+        color: #F9F5F0;
+    }
+</style>
