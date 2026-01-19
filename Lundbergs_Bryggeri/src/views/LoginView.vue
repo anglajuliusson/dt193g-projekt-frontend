70efc0360d4ae5c67e56f256048f4479d206a602
Ängla Juliusson <angla02@icloud.com>
2026-01-19 11:16:19 +0100

    importera loginForm komponenten och skriv ut på login-vyn

--- Lundbergs_Bryggeri/src/views/LoginView.vue
+++ Lundbergs_Bryggeri/src/views/LoginView.vue
@@ -1,7 +1,27 @@
-<script setup></script>
+<script setup>
+    import LoginForm from '../components/LoginForm.vue'
+
+</script>
   
 <template>
-
+    <main>
+        <h1>Lundbergs Bryggeri</h1>
+        <h2>Admin login</h2>
+        <LoginForm />
+    </main>
 </template>
   
-<style scoped></style>
+<style scoped>
+    main {
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+        color: #3E3E3E;
+    }
+    h1 {
+        margin-top: 3em;
+    }
+    h2 {
+        margin-top: 1em;
+    }
+</style>
