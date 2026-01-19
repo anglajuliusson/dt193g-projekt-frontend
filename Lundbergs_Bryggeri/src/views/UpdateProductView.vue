a74b3946a69ab83d412e84049fea96fc3673ed4a
Ängla Juliusson <angla02@icloud.com>
2026-01-19 14:00:28 +0100

    UpdateProductView med hämtning av produktdata via ID

--- Lundbergs_Bryggeri/src/views/UpdateProductView.vue
+++ Lundbergs_Bryggeri/src/views/UpdateProductView.vue
@@ -1,4 +1,23 @@
-<script setup></script>
+<script setup>
+    import { ref, onMounted } from 'vue';
+    import { useRoute } from 'vue-router';
+
+    const route = useRoute();
+    const productId = route.params.id; // id som skickades via router.push
+    const product = ref({});
+
+    const fetchProduct = async () => {
+    const token = localStorage.getItem('token');
+    const res = await fetch(`http://localhost:3000/products/${productId}`, {
+        headers: { 'Authorization': `Bearer ${token}` }
+    });
+    product.value = await res.json();
+    };
+
+    onMounted(() => {
+    fetchProduct();
+    });
+</script>
   
 <template>
 
