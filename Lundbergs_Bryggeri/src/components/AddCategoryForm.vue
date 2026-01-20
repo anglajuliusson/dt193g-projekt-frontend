42956c404cf6a4802d5e4b3586dae76da300122e
Ängla Juliusson <angla02@icloud.com>
2026-01-20 10:47:23 +0100

    Fungerande formulär för att lägga till ny kategori i rätt vy

---
+++ Lundbergs_Bryggeri/src/components/AddCategoryForm.vue
@@ -0,0 +1,61 @@
+<script setup>
+    import { ref } from 'vue';
+    
+    // Skapar ett custom event som skickar formulärdata till föräldrakomponenten
+    const emit = defineEmits(['submit']);
+    
+    // Reaktivt objekt som lagrar värdena från formuläret
+    // Används för att binda inputfält via v-model
+    const formData = ref({
+      name: ''
+    });
+    
+    // Körs när formuläret skickas
+    // Skickar formData till föräldern via emit
+    const submitForm = () => {
+      emit('submit', formData.value);
+    };
+</script>
+    
+    <template>
+      <form @submit.prevent="submitForm" class="form-container">
+        <div class="input-group">
+          <label>Namn:</label>
+          <input v-model="formData.name" placeholder="Namn" required />
+        </div>
+        <button type="submit">Lägg till kategori</button>
+      </form>
+    </template>
+   
+<style scoped>
+    .form-container {
+        display: flex;
+        flex-direction: column;
+        max-width: 400px;
+        width: 90%;
+        margin: 0 auto;
+        text-align: left;
+        margin-top: 2em;
+    }
+    .input-group {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 1.5em;
+    }
+    label {
+        margin-bottom: 0.5em;
+    }
+    input, textarea {
+        padding: 5px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+    button {
+        background-color: #B87333;
+        color: #3e3e3e;
+        border-radius: 5px;
+        border: none;
+        padding: 5px;
+        margin-bottom: 3em;
+    }
+</style>
--- Lundbergs_Bryggeri/src/views/AddCategoryView.vue
+++ Lundbergs_Bryggeri/src/views/AddCategoryView.vue
@@ -1,7 +1,47 @@
-<script setup></script>
-  
+<script setup>
+    import { useRouter } from 'vue-router';
+    import CategoryFrom from '../components/AddCategoryForm.vue';
+    
+    // Router används för att nevigera mellan vyer efter lyckat anrop
+    const router = useRouter();
+    
+    // Funktion som tar emot formulärdata från ProductForm-komponenten
+    // och skickar ett POST-anrop till backend för att skapa en ny produkt
+    const handleSubmit = async (formData) => {
+      // Hämtar JWT-token från localStorage för autentisering
+      const token = localStorage.getItem('token');
+    
+      // Skickar POST-anrop till backend med produktdata
+      const res = await fetch('http://localhost:3000/categories', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}` // Token krävs för skyddad endpoint
+        },
+        body: JSON.stringify(formData)
+      });
+    
+      // Vid lyckat svar navigeras användaren tillbaka till produktlistan
+      if (res.ok) {
+        router.push('/categories');
+      } else {
+        const data = await res.json();
+        alert("Fel vid tillägg: " + data.error);
+      }
+    };
+</script>    
+    
 <template>
-
-</template>
+    <main>
+        <h1>Lägg till ny kategori</h1>
+        <CategoryFrom @submit="handleSubmit" />
+    </main>
+</template>    
   
-<style scoped></style>
+<style scoped>
+    main {
+        text-align: center;
+        margin-top: 2em;
+        margin-bottom: 3em;
+    }
+</style>
