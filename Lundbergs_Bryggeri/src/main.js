1bd01e7e4ee406aea200673347491d647abba48d
Ängla Juliusson <angla02@icloud.com>
2026-01-21 10:47:21 +0100

    Installera Bulma och importera i main.js

--- Lundbergs_Bryggeri/src/main.js
+++ Lundbergs_Bryggeri/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "./assets/main.css"; // Inkludera CSS-fil
+import 'bulma/css/bulma.css'; // Importera Bulma
 
 const app = createApp(App)
 
