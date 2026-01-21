b335ba22609cefd9e0577c9dc01be137a2be0c1b
Ängla Juliusson <angla02@icloud.com>
2026-01-21 10:21:35 +0100

    Dölj header-komponent på login-sidan

--- Lundbergs_Bryggeri/src/App.vue
+++ Lundbergs_Bryggeri/src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
+    import { useRoute } from 'vue-router';
     // Importerar gemensamma layout-komponenter som ska visas på alla sidor i applikationen.
     import Header from './components/Header.vue' // Header-komponenten
+
+  const route = useRoute();
 </script>
 
 <template>
   <!-- Huvudlayout för hela applikationen. 
+       Header visas inte på login-sidan
        Innehållet i <router-view /> byts dynamiskt beroende på vilken route som är aktiv. -->
-       <Header />
+       <Header v-if="route.name !== 'login'" />
        <router-view />
 </template>
 
